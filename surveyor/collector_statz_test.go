2d072f6abda266ee0e4ef0598ad6ad6c1e5b02c2
Waldemar Quevedo <wally@nats.io>
2026-01-23 00:11:30 -0800

    Add JetStream meta snapshot metrics (#315)
    Adds new Prometheus metrics for JetStream meta snapshot monitoring:
    
    - nats_core_jetstream_meta_snapshot_pending_entries
    - nats_core_jetstream_meta_snapshot_pending_bytes
    - nats_core_jetstream_meta_snapshot_last_duration
    
    Signed-off-by: Waldemar Quevedo <wally@nats.io>

--- surveyor/collector_statz_test.go
+++ surveyor/collector_statz_test.go
@@ -412,6 +412,55 @@ func TestStatzCollector_GoMemLimit(t *testing.T) {
 	}
 }
 
+func TestStatzCollector_WithStats_JetstreamMetaSnapshot(t *testing.T) {
+	statsRaw, err := os.ReadFile("testdata/stats/stats-meta-snapshot.json")
+	if err != nil {
+		t.Fatalf("error reading testdata: %v", err)
+	}
+	stats := &server.ServerStatsMsg{}
+	err = json.Unmarshal(statsRaw, stats)
+	if err != nil {
+		t.Fatalf("error unmarshalling stats: %v", err)
+	}
+
+	sc, err := NewStatzCollectorOpts(
+		WithStats(WithStatsBatch{
+			Stats: []*server.ServerStatsMsg{stats},
+		}),
+	)
+	if err != nil {
+		t.Fatalf("error creating statz collector: %v", err)
+	}
+
+	output := gatherStatzCollectorMetrics(t, sc)
+
+	// Verify all JetStream meta cluster snapshot metrics are present
+	want := []string{
+		"nats_core_jetstream_meta_snapshot_pending_entries",
+		"nats_core_jetstream_meta_snapshot_pending_bytes",
+		"nats_core_jetstream_meta_snapshot_last_duration",
+	}
+
+	for _, m := range want {
+		if !strings.Contains(output, m) {
+			t.Fatalf("invalid output, missing '%s':\n%v\n", m, output)
+		}
+	}
+
+	// Verify specific metric values
+	expectedValues := []string{
+		"nats_core_jetstream_meta_snapshot_pending_entries{cluster_name=\"meta-cluster\",server_id=\"meta-server1\",server_name=\"meta-server1\"} 1500",
+		"nats_core_jetstream_meta_snapshot_pending_bytes{cluster_name=\"meta-cluster\",server_id=\"meta-server1\",server_name=\"meta-server1\"} 524288",
+		"nats_core_jetstream_meta_snapshot_last_duration{cluster_name=\"meta-cluster\",server_id=\"meta-server1\",server_name=\"meta-server1\"} 1.23456789e+09",
+	}
+
+	for _, expectedValue := range expectedValues {
+		if !strings.Contains(output, expectedValue) {
+			t.Fatalf("expected metric value not found. Expected: %s\nActual output:\n%v", expectedValue, output)
+		}
+	}
+}
+
 func TestStatzCollector_MetricInfos(t *testing.T) {
 	sc, err := NewStatzCollectorOpts(
 		WithStats(WithStatsBatch{
